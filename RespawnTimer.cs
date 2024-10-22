867376f06138f67a501da1179a4131b18fa56f5b
Dollan <dollan@duck.com>
2024-10-22 16:22:27 +0200

    First commit

---
+++ RespawnTimer.cs
@@ -0,0 +1,52 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Core.Translations;
+using Microsoft.Extensions.Logging;
+
+namespace RespawnTimer;
+public class RespawnTimer : BasePlugin
+{
+    public override string ModuleName => "RespawnTimer";
+    public override string ModuleDescription => "Respawn Timer for Minigames";
+
+    public override string ModuleVersion => "1.0.0";
+    public override string ModuleAuthor => "dollan";
+
+    public bool DoRespawn = false;
+
+    public override void Load(bool hotReload)
+    {
+        Logger.LogInformation("RespawnTimer loaded");
+    }
+
+    [GameEventHandler]
+    public HookResult RoundStart(EventRoundStart @event, GameEventInfo info)
+    {
+        DoRespawn = true;
+        AddTimer(60, () => DoRespawn = false);
+        return HookResult.Continue;
+    }
+
+    public void DisableRespawn()
+    {
+        DoRespawn = false;
+        Server.PrintToChatAll(StringExtensions.ReplaceColorTags("{Lime}[Minigames] {Red}Respawn disabled"));
+    }
+
+    [GameEventHandler]
+    public HookResult PlayerDeath(EventPlayerDeath @event, GameEventInfo info)
+    {
+        if (DoRespawn)
+        {
+            CCSPlayerController? user = @event.Userid;
+            if (user != null)
+            {
+                user.Respawn();
+                user.PrintToChat(StringExtensions.ReplaceColorTags("{Lime}[Minigames] {Red}Respawned"));
+            }
+            return HookResult.Continue;
+        }
+        return HookResult.Continue;
+    }
+}
